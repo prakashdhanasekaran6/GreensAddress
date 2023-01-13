0e9f86a07642e370a51d0c20b3cb14ffcac52789
prakashdhanasekaran6 <prakashdhanasekaran6@gmail.com>
2023-01-12 23:40:05 -0500

    pushing greenaddress

---
+++ src/org/add/GreenTeach.java
@@ -0,0 +1,24 @@
+package org.add;
+
+public class GreenTeach {
+private void greensOmr() {
+	System.out.println("Green address is no:1 2nd street omr chennai-01");
+}
+private void greensTambaram() {
+	System.out.println("Green address is no.2 3rd street tambaram chennai-02");
+
+}
+private void greensVelacherry() {
+	System.out.println("Green address is no.3 4th street velacherry chennai-03");
+}
+private void greensAnnanagar() {
+	System.out.println("Green address is no.4 5th street annanagar chennai-04");
+}
+public static void main(String[] args) {
+	 GreenTeach greenTechClass = new  GreenTeach();
+	 greenTechClass.greensOmr();
+	 greenTechClass.greensTambaram();
+	 greenTechClass.greensVelacherry();
+	 greenTechClass.greensAnnanagar();
+}
+}
